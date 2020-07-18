8b3ce2ede25ba813c8c4eb94d648036358ea1e5e
Sinisa88 <sinisamikolic@gmail.com>
2020-07-18 13:47:13 +0200

    BW Mod Klase
    Sve klase u Balkan Warlords Modu

---
+++ klase.cpp
@@ -0,0 +1,117 @@
+Klase po kategorijama / ClassNames by Catogory:
+
+
+// Pića / Drinks
+BW_Votka
+BW_Rakija_Plasticna
+BW_Sok_Jabuka
+BW_Studenac
+BW_Osjecajno
+BW_Sarajevsko
+BW_Hell
+BW_Kruskovac
+
+
+// Jela / Food
+BW_Cevapi
+BW_Grah_Opaki
+BW_Kompot
+BW_Spageti
+BW_Sarma
+BW_Paprika
+// NOVO
+BW_Cokolino_co
+BW_Cedevita_co
+
+
+// Plakati / Billboards
+BW_Plakat_Pivo
+BW_Plakat_Pivo_Sar
+BW_Plakat_Croatia
+BW_Plakat_Survivor_City
+BW_Plakat_Balkan_Warlords
+BW_Plakat_Campfire
+
+// Bankomat
+BW_Bankomat
+
+// Torbe / Backpacks
+BW_Alice_Torba
+BW_Torba_Armija_BiH
+BW_Torba_Camo1
+BW_Torba_Vojska_HRV
+BW_Torba_Camo2
+BW_Torba_SRB
+
+// Za glavu/Head Gear
+BW_Sesiric_CrnoCrveni
+Balaclava_Death
+BW_Sesiric_Camo
+BW_Balaclava_CamoSkull
+BW_Balaklava_3Rupe_DP
+BW_Sesiric_Armija_BiH
+BW_Sesiric_Camo1
+BW_Sesiric_Vojska_HRV
+BW_Sesiric_Camo2
+BW_Sesiric_SRB
+
+
+// Majice-Košulje-Dukse / Tops
+BW_Majica_Bijela_Punisher
+BW_Majica_Camo
+BW_Majica_Osjecajno
+BW_Majica_Superman
+BW_Majica_F
+BW_Majica_Porno
+BW_Zatvorenik_Kosulja
+BW_Duksa_FU
+BW_Majica_Jebivetar
+BW_Majica_Betmen
+
+// Jakne / Jackets
+BW_Kozna_Jakna_IronMaiden
+BW_RidersJacket_MCInfected
+BW_M65_Jakna_Skull
+BW_M65_Jakna_Camo
+BW_Jakna_Camo
+BW_Jakna_Crna_Camo
+BW_Jakna_Crna_BiH
+BW_Jakna_Crna_HRV
+BW_Jakna_Crna_Punisher
+BW_Armija_BiH_Jakna
+BW_Camo1_Jakna
+BW_Vojska_HRV_Jakna
+BW_Jakna_Camo2
+BW_Jakna_SRB
+
+// Hlaće / Pants
+BW_Hlace_Camo
+BW_Zatvorenik_Hlace
+BW_Hlace_CrnoCrvena
+BW_Hlace_Armija_BiH
+BW_Hlace_Camo1
+BW_Hlace_Vojska_HRV
+BW_Hlace_Camo2
+BW_Hlace_SRB
+
+// Obuća / Footwear
+BW_Fahirine_Cizme
+BW_Camo_Cizme
+BW_Patike_Bijele
+
+// Prsluci
+BW_Prsluk_BiH
+BW_Prsluk_Camo1
+BW_Prsluk_HRV
+BW_Prsluk_Camo2
+BW_Vesta_SRB
+
+// Oružje / Firearms
+BW_AKM_Neon
+BW_M4_Camo
+BW_M4_Carbon_Bijeli
+BW_M4_Carbon_Crni
+BW_AKM_Camo
+BW_Winchester_Punisher
+BW_Winchester_Camo
+BW_UMP_Orange
